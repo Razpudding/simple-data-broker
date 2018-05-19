e37cf36e5d74c48103d543236db2924a4d48e33b
Laurens <laurens.aarnoudse@gmail.com>
2018-05-19 17:02:00 +0200

    Added usage documentation. Added error logging in case request is invalid, malformed.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # simple-data-broker
 This Node server is a simple data broker which accepts POST requests and stores data in a database.
+
+## Usage
+
+The server accepts two requests:
+
+1.`POST /` If a message is posted to the root of the server following this format: {id:Number, status:Number, [additional optional params]} the server will store the message in a database table as is. Both the `id` and the `status` are required. The request should be a valid POST request with all data values provided as query parameters. After testing I found out the following content types will work (application/x-www-form-urlencoded, multipart/form-data, no content-type provided) although i'm not 100% sure on that last one.
+2. `GET /` If the root of the server is requested, all available data is dumped back to the requester. Note: I have no idea how stable that is or how long the data is allowed to be for this to work. This is purely a test feature and should prob be protected.
--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 /*
 * Created by: Laurens (github.com/razpudding)
+* After running, test by using postman or httpie( http -v POST :3000 hello=='World')
 */
 
 /*
 * Try node-inspector for server side debugging
+* Use something like this to render the data on get client-side / https://github.com/caldwell/renderjson
 */
 
 const express = require('express')        //used to create a webserver
-const bodyParser = require('body-parser') //used for parsing requests for data
 
 //Will need this later for storing secret stuff
 //require('dotenv').config()
 
 //This is gonna hold our data for now. Will be moved to a database later
-let data = [
-  {
-  },
-]
+let data = []
 
 //This sets up our webserver using the express package
 express()
@@ -28,17 +26,28 @@ express()
 
 //All this does is show clientside the server is working
 function serveHome(req, res){
-  res.send("Move along sir, nothing to see here")
+  data.length ? res.send(data) : res.send("Move along sir, nothing to see here")
 }
 
 //This function captures the query in the url and saves it to a local data array
+//Any data is accepted as long as an 'id' and 'status' are provided with the request
 function writeData(req, res){
-  let input = req.query  //Capture the query in the request
-  console.log(input)
-  input.id = data.length
-  data.push(input)
-  console.log(data)
-  res.status(200)
-  res.send()
+  try {
+    let input = req.query  //Capture the query in the request
+    console.log(req.url.length)
+    if (req.headers['content-length'] > 100 || req.url.length > 5000) {throw 'Request too large to process'};
+    if (!input.id) {throw 'No request id provided'}
+    if (!input.status) {throw 'No status provided in message'}
+    //console.log(input)
+    input.messageId = data.length
+    data.push(input)
+    console.log(data)
+    res.status(200)
+    res.send(data)
+  }
+  catch(error){
+    console.log("Client didn't provde the right arguments for the request", error)
+    res.status(406)
+    res.send(error)
+  }
 }
-
