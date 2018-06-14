fee90549cf99aefeb9d14d746b4ddc43420e3710
sjoerdbeentjes <mail@sjoerdbeentjes.nl>
2018-06-14 11:35:51 +0200

    create seperate view for month overview

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -1,83 +1,13 @@
 <template>
-  <v-container class="full-height" fluid grid-list-xl>
-    <v-select
-      :items="years"
-      v-model="selectedYear"
-      label="Select"
-      item-value="text"
-      y-offset
-      single-line
-    ></v-select>
-
-    <v-layout class="full-height" row wrap>
-      <v-layout v-if="loading" justify-center>
-        <v-progress-circular indeterminate color="primary"></v-progress-circular>
-      </v-layout>
-
-      <v-alert v-if="error" :value="true" type="error">
-        Something went wrong
-      </v-alert>
-
-      <div class="empty-state" v-if="!loading && !months.length">
-        <p>No data found</p>
-      </div>
-
-      <v-flex v-else-if="!loading" v-for="month in months" :key="month.name" xs12 sm6 md3 xl2>
-        <MonthCard :month="month"/>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <MonthOverview />
 </template>
 
 <script>
-import MonthCard from './MonthCard.vue'
-import axios from 'axios'
+import MonthOverview from './MonthOverview.vue'
 
 export default {
   components: {
-    MonthCard
-  },
-  created() {
-    this.getMonths();
-  },
-  data() {
-    return {
-      loading: false,
-      error: null,
-      selectedYear: '2018',
-      months: [],
-      years: [
-        { text: '2018' },
-        { text: '2017' },
-        { text: '2016' }
-      ]
-    }
-  },
-  watch: {
-    selectedYear() {
-      this.getMonths();
-    }
-  },
-  methods: {
-    getMonths() {
-      this.loading = true;
-
-      axios.get(`http://localhost:3000/api/months?year=${this.selectedYear}`)
-        .then(res => {
-          this.months = res.data;
-          this.loading = false;
-        })
-        .catch(err => {
-          this.loading = false;
-          this.error = err;
-        })
-    }
+    MonthOverview
   }
 }
 </script>
-
-<style scoped>
-  .input-group--select {
-    max-width: 10rem;
-  }
-</style>
